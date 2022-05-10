5ebc31cb08f9fc32ee2f71955cec467f2841fced
loui9583 <loui9583@stud.kea.dk>
2022-05-10 13:36:01 +0200

    MemberList i csv fil

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-      Controller controller = new Controller();
-
-      MemberListNStuff memberListNStuff = new MemberListNStuff();
-
-      memberListNStuff.loadMenu();
-
-      memberListNStuff.addMember();
-
-      memberListNStuff.showMenu();
-
 
 
    // controller.run();
