90e51ba2fc3f4c9014b6ef812da1f8eb1b3ff7d2
adrnl1ne <jako7096@stud.kea.dk>
2022-05-10 12:48:58 +0200

    menu update

--- src/Controller.java
+++ src/Controller.java
@@ -6,8 +6,12 @@ public class Controller {
   FileReader fileReader = new FileReader();
   Music music = new Music();
   private boolean loop = true;
+  private final String boldON = "\033[0;1m";
+  private final String boldOff = "\033[0;0m";
+  private static final String TEXT_RESET = "\u001b[0m";
+  private static final String TEXT_GREEN = "\u001b[32m";
 
-  public void run() throws IOException, InterruptedException {
+  public void run() throws InterruptedException {
     music.playMusic();
     Thread.sleep(2000);
     while (loop) {
@@ -16,8 +20,16 @@ public class Controller {
 
   }
 
-  public void mainMenu() throws IOException {
-    System.out.println(fileReader.mainMenu());
+  public void mainMenu() {
+    System.out.printf(boldON + TEXT_GREEN + "[%s]\n", "-".repeat(47));
+    System.out.printf("|     " + TEXT_RESET + "Velkommen til svømmeklubben Delfinen!" + TEXT_GREEN + "     |" + "\n"
+        + "|      " + TEXT_RESET + "Vælg venligst en funktion nedenfor:" + TEXT_GREEN + "      |" + "\n" +
+        "|                                               |" + "\n" +
+        "|" + TEXT_RESET + "1)" + TEXT_GREEN + "                                             |" + "\n" +
+        "|" + TEXT_RESET + "2)" + TEXT_GREEN + "                                             |" + "\n" +
+        "|" + TEXT_RESET + "3)" + TEXT_GREEN + "                                             |" + "\n" +
+        "|" + TEXT_RESET + "4) Exit" + TEXT_GREEN + "                                        |" + TEXT_RESET);
+    System.out.printf("\n" +TEXT_GREEN + "[%s]\n", "-".repeat(47));
     switch (gui.getInt()) {
       case 1 -> System.out.println("something");
       case 2 -> System.out.println("something else");
