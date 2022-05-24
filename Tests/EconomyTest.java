814b81cfb0b245d3a53812bf30c0939991e8719f
louis <loui9583@stud.kea.dk>
2022-05-24 11:16:15 +0200

    =?

---
+++ Tests/EconomyTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EconomyTest {
+
+    Economy economy = new Economy();
+
+
+    KonkurrenceManager loadKonkurrenceManager() throws FileNotFoundException, ParseException {
+        KonkurrenceManager konkurrenceManager = new KonkurrenceManager();
+        konkurrenceManager.loadMemberFile();
+        return konkurrenceManager;
+    }
+    MotionistManager loadMotionistManager() throws FileNotFoundException, ParseException {
+        MotionistManager motionistManager = new MotionistManager();
+        motionistManager.loadMenu();
+        return motionistManager;
+    }
+
+    // Not sure if done right, maybe too much logic code in the test-method?
+    @Test
+    void getExpectedIncomeTest() throws FileNotFoundException, ParseException {
+        int expectedIncome = economy.getExpectedIncome(loadKonkurrenceManager(), loadMotionistManager());
+        int expectedIncomeTestResult = 0;
+        for (Member member:loadMotionistManager().memberList
+             ) {
+            expectedIncomeTestResult += member.getKontingent();
+        }
+        for (MemberKonkurrence member: loadKonkurrenceManager().memberList
+             ) {
+            expectedIncomeTestResult += member.getKontingent();
+        }
+
+        assertEquals(expectedIncome,expectedIncomeTestResult);
+    }
+
+    @Test
+    void getActualIncomeTest() throws FileNotFoundException, ParseException {
+        int actualIncome = economy.getActualIncome(loadKonkurrenceManager(),loadMotionistManager());
+        int actualIncomeTestResult = 0;
+        for (Member member:loadMotionistManager().memberList
+        ) {
+            if (member.getBetalt().equals("betalt")) {
+                actualIncomeTestResult += member.getKontingent();
+            }
+        }
+        for (MemberKonkurrence member: loadKonkurrenceManager().memberList
+        ) {
+            if (member.getBetalt().equals("betalt")) {
+                actualIncomeTestResult += member.getKontingent();
+            }
+        }
+
+        assertEquals(actualIncome, actualIncomeTestResult);
+    }
+
+}
